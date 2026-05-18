8d50bcf37f164b6e8c701cdfbd4dda4800edba53
EYing54 <lokipdefin947@gmail.com>
2026-05-18 09:47:39 +0800

    Update config.py

--- Flask/config.py
+++ Flask/config.py
@@ -7,8 +7,7 @@ load_dotenv()
 # 从.env获取账号和密码
 db_user = os.getenv("DB_USER")
 db_pwd = os.getenv("DB_PASSWORD")
+db_na = os.getenv("DB_NAME")
 
 # 最终的连接
-SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql://{db_user}:{db_pwd}@127.0.0.1:3306/smart_interview_db"
-)
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_pwd}@127.0.0.1:3306/{db_na}"
