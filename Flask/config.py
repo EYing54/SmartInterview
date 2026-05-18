858e1c41b8cd6a0599642c949651885faa2513b7
EYing54 <lokipdefin947@gmail.com>
2026-05-18 09:39:31 +0800

    Update config.py

--- Flask/config.py
+++ Flask/config.py
@@ -1,2 +1,14 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:313708@localhost:3306/smart_interview_db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+import os
+from dotenv import load_dotenv
+
+# 加载.env文件
+load_dotenv()
+
+# 从.env获取账号和密码
+db_user = os.getenv("DB_USER")
+db_pwd = os.getenv("DB_PASSWORD")
+
+# 最终的连接
+SQLALCHEMY_DATABASE_URI = (
+    f"mysql+pymysql://{db_user}:{db_pwd}@127.0.0.1:3306/smart_interview_db"
+)
