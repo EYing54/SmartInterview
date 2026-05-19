417391b46296a6f9cdc499b0ab508e7989328c51
EYing54 <lokipdefin947@gmail.com>
2026-05-19 18:35:55 +0800

    Update question.py
    修改了查询到的题目的返回的数据类型

--- Flask/app/routes/question.py
+++ Flask/app/routes/question.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request  # noqa: F401
+from flask import Blueprint, request, jsonify  # noqa: F401
 from app.models.question import QuestionBank
 from extensions import db
 from datetime import datetime
@@ -9,7 +9,17 @@ question_bp = Blueprint("question", __name__)
 @question_bp.route("/query_question")
 def query_question():
     questions = QuestionBank.query.all()
-    return str(questions)
+    result_list = []
+    for i in questions:
+        i_dict = {
+            "question_id": i.question_id,
+            "question": i.question,
+            "answer": i.answer,
+            "is_deleted": i.is_deleted,
+            "create_time": i.create_time,
+        }
+        result_list.append(i_dict)
+    return jsonify({"code": 200, "msg": "题目查询成功！", "data": result_list})
 
 
 @question_bp.route("/add_question")
